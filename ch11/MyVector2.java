b92af4019a2652f37f04285b578dc0f6c4691863
gototheTK <gototkno1@gmail.com>
2022-07-02 22:34:10 +0900

    comparator

---
+++ ch11/MyVector2.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+
+/**
+ * MyVector2
+ */
+public class MyVector2 extends MyVector implements Iterator{
+
+    int cursor = 0;
+    int lastRet = -1;
+
+    public MyVector2(int capacity){
+        super(capacity);
+    }
+
+    public MyVector2() {
+        this(10);
+    }
+
+    public String toString() {
+
+        String tmp ="";
+        Iterator it = iterator();
+
+        for(int i=0; it.hasNext(); i++){
+
+            if(i!=0) tmp+=", ";
+            tmp += it.next();   //  tmp +=next().toString();
+
+        }
+
+        return "[" + tmp +"]";
+
+    }
+
+    public Iterator iterator() {
+        cursor = 0;
+        lastRet = -1;
+        return this;
+    }
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return cursor != size();
+    }
+
+    @Override
+    public Object next() {
+        // TODO Auto-generated method stub
+
+        Object next = get(cursor);
+        lastRet = cursor++;
+
+        return next;
+    }
+
+    public void remove() {
+
+        //  더이상 삭제할 것이 없으면 IllegalStateException을 발생시킨다.
+        if(lastRet==-1) {
+            throw new IllegalStateException();
+        } else {
+            remove(lastRet);
+            // cursor--;   //  삭제 후에 cursor의 위치를 감소시킨다.
+            lastRet = -1;   //  lastRet의 값을 초기화한다.
+        }
+
+    }
+    
+}   // class
