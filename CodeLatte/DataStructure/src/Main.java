804a0d2832a4b2e58bfe01df5fc78264e97f90ec
CH Jo <48229162+gototheTK@users.noreply.github.com>
2023-02-23 23:12:42 +0000

    Generic

---
+++ CodeLatte/DataStructure/src/Main.java
@@ -0,0 +1,20 @@
+public class Main 
+
+  public static void main(String[] args) {
+    String[] array = new String[500000];
+
+    System.out.println("데이터 사입 시작. 잠시만 기다려주세요.");
+    // TODO : 배열에 값을 저장한다.
+
+    
+    for (int i =0; i < array.length; i++) {
+      int x = (int) (Math.random() * array.length);
+      int y = (int) (Matn.random())
+    }
+
+
+    
+    
+  }
+  
+}
