2c7ee997d9d1175d1fcd484fcd09229fea4c816d
CH Jo <48229162+gototheTK@users.noreply.github.com>
2023-03-01 03:58:53 +0000

    ThreadExample01

--- CodeLatte/Java/src/com/codelatte/thread/ThreadEx01.java
+++ CodeLatte/Java/src/com/codelatte/thread/ThreadEx01.java
@@ -1,3 +1,57 @@
+public class Main {
 
+  static class ThreadTest implements Runnable{
 
-public class 
+    private String name;
+    private Integer count;
+
+    public ThreadTest(String name, Integer count){
+
+      this.name = name;
+      this.count = count;
+      
+    }
+
+    public void run() {
+
+      int result = 0;
+
+      int count = this.count.intValue();
+
+      for(int i = 0; i< count; i++) {
+
+        result +=1;
+        
+      }
+
+      System.out.println(this.name +":" + result);
+      
+    }
+
+    
+  }
+
+  public static void main(String[] args) {
+
+    try {
+        
+        Thread test1 = new Thread(new ThreadTest("A", 100000000));
+        Thread test2 = new Thread(new ThreadTest("B", 10000000));
+        
+        test1.setDaemon(true);
+        test2.setDaemon(true);
+    
+        test1.start();
+        test2.start();
+        
+        test1.join();
+        test2.join();
+        
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    } 
+    
+    
+  }
+  
+}
