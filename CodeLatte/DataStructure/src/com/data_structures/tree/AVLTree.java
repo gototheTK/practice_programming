4bada75ec6243986593b1a339748a903060c0127
CH Jo <48229162+gototheTK@users.noreply.github.com>
2023-03-14 12:12:14 +0000

    Data_Structures/AVLTree.java

--- CodeLatte/DataStructure/src/com/data_structures/tree/AVLTree.java
+++ CodeLatte/DataStructure/src/com/data_structures/tree/AVLTree.java
@@ -3,6 +3,7 @@ package com.data_structures.tree
 
 class Node {
   int key;
+  int height;
   Node left;
   Node right;
 
@@ -57,7 +58,7 @@ public class AVLTree {
         Node predecessor = getLargestNode(root.left);
         int removeKey = root.key;
         root.key = predecessor.key;
-        predecessor.key =removeKey;
+        predecessor.key = removeKey;
         root.left = removedNode(root.left, key);
         
       } else if(null!=root.right) {
@@ -69,7 +70,7 @@ public class AVLTree {
         root.right = removedNode(root.right, key);
         
       } else {
-        root = null
+        root = null;
       }
       
     }
@@ -78,7 +79,102 @@ public class AVLTree {
     
   }
 
+  private Node rotate(Node x) {
+
+    int xNodeBalance = getBalance(x);
+
+    if (Math.abs(xNodeBalance >= 2) {
+
+      if (xNodeBalance > 1 && 0 <= getBalance(x.left)) {
+        x = LL_rotate(x);
+      } else if (xNodeBalance > 1 && -1 == getBalance(x.left)) {
+        x.left = RR_rotate(x.left);
+        x = LL_rotate(X);
+      } else if(xNodeBalance < -1 && 0 >= getBalance(x.right)) {
+        x = RR_rotate(x);
+      } else if(xNodeBalance < -1 && 1 == getBalance(x.right)) {
+        x.right = LL_rotate(x.right);
+        x = RR_rotate(x);
+      }
+    }
+
+    return x;
+    
+  }
+
+  private Node getLargestNode(Node x){
+    if(null == x.right){
+      return x;
+    }
+    return getLargestNode(x.right);
+  }
+
+  private Node getSmallestNode(Node x) {
+    if(null == x.left) {
+      return x;
+    }
+    return getSmallestNode(x.left);
+  }
+
+  public int search(int key) {
+    return searchNode(root, key).key;
+  }
+
+  private Node searchNode(Node x, int key) {
+    Node node = x;
+    if (null == node) {
+      throw new RuntimeException(" 키 를 찾 을 수 없 습 니 다.");
+    } else if (node.key > key) {
+      node = searchNode(x.left, key);
+    } else if (node.key < key) {
+      node = searchNode(x.right, key);
+    }
+    return node;
+  }
   
+
+  private Node LL_rotate(Node P) {
+    Node L = P.left;
+    Node LR = L.right;
+    L.right = P;
+    P.left = LR;
+
+    changeNodeHeight(P);
+    changeNodeHeight(L);
+
+    return L;
+    
+  }
+
+  private Node RR_rotate(Node P) {
+
+    Node R = P.right;
+    Node RL = R.left;
+    R.left = P;
+    P.right = RL;
+
+    changeNodeHeight(P);
+    changeNodeHeight(R);
+
+    return R;
+    
+  }
+
+  private int getHeight(Node x) {
+    int leftChildHeight = (null != x.left) ? x.left.height : -1;
+    int rightChildHeight = (null != x.right) ? x.right.height : -1;
+    return Math.max(leftChildHeight, rightChildHeight) +1;
+  }
+
+  private int getBalance(Node x) {
+    int leftChildHeight = (null != x.left) ? x.left.height : -1;
+    int rightChildHeight = (null != x.right) ? x.right.height : -1;
+    return leftChildHeight - rightChildHeight;
+  }
+
+  private void changeNodeHeight(Node x) {
+    x.height = getHeight(x);
+  }
 
   
 }
